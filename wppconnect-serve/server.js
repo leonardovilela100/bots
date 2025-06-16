5be4853ac14666c5692590cfa9030465490d6888
Leonardo Carvalho Vilela <leonardo.vilela_ext@light.com.br>
2025-06-15 22:45:34 -0300

    Feat: Add gitignore

--- wppconnect-serve/server.js
+++ wppconnect-serve/server.js
@@ -1,13 +1,19 @@
 const wppconnect = require('@wppconnect-team/wppconnect');
 const express = require('express');
 
+let lastQrData = null;
 // Initialize WhatsApp connection and then start the REST server
 wppconnect
   .create({
     session: 'server-session',
+    catchQR: (base64Qr, asciiQR, attempts, urlQR) => {
+      lastQrData = { base64Qr, asciiQR, attempts, urlQR };
+    },
   })
   .then((client) => {
     const app = express();
+    console.log(client);
+    console.log(app);
     app.use(express.json());
 
     // Endpoint to send text messages via POST
@@ -25,9 +31,17 @@ wppconnect
       }
     });
 
+    app.get('/qr-code', (req, res) => {
+      if(!lastQrData){
+        return res.status(404).json({ error: 'QrCode not gerenated yet' });
+      }
+      res.json(lastQrData);
+    });
+
     const port = process.env.PORT || 3000;
     app.listen(port, () => {
       console.log(`wppconnect server listening on ${port}`);
+      console.log(port);
     });
   })
   .catch((error) => {
