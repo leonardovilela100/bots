c7fc8f664bf7d831462ac2468782122457de5dd3
Leonardo Carvalho Vilela <leonardo.vilela_ext@light.com.br>
2025-08-25 20:59:44 -0300

    Add escuta de msg

--- wppconnect-serve/listeners/messageListener.js
+++ wppconnect-serve/listeners/messageListener.js
@@ -2,6 +2,12 @@ function registerMessageListener(client) {
   client.onMessage((message) => {
     console.log('Received message:', message);
   });
+/*
+  client.onMessage(async (message) => {
+    if (message.fromMe) return;
+    await client.sendText(message.from, 'Eu sou Leonardo, como posso te ajudar?');
+  });
+  */
 }
 
 module.exports = { registerMessageListener };
